325bb07686b04497a56bd93adf579dae7f18c717
Emma Long <emmag2623@gmail.com>
2025-02-16 22:47:54 -0500

    outline for level scene

--- src/scenes/TitleScene.js
+++ src/scenes/TitleScene.js
@@ -1,6 +1,6 @@
 import Phaser from 'phaser'
 import startImg from '../assets/images/start-btn.png'
-import titleImg from '../assets/images/title-bg.png'
+import titleBgImg from '../assets/images/title-bg.png'
 
 
 export default class TitleScene extends Phaser.Scene {
@@ -10,23 +10,16 @@ export default class TitleScene extends Phaser.Scene {
 
     preload() {
         // load iamges
-        this.load.image('background', titleImg)
+        this.load.image('titleBackground', titleBgImg)
         this.load.image('startButton', startImg)
     }
 
     create() {
         // add background image, centered
-        this.add.image(this.scale.width / 2, this.scale.height / 2, 'background')
-            .setOrigin(0.5)
-            .setDisplaySize(this.scale.width, this.scale.height)
+        this.add.image(this.scale.width / 2, this.scale.height / 2, 'titleBackground').setOrigin(0.5).setDisplaySize(this.scale.width, this.scale.height)
 
         // add start button
-        const startButton = this.add.image(185, 450, 'startButton')
-            .setScale(.65)
-            .setInteractive({
-                userHandCursor: true,
-                pixelPerfect: true
-            }) // make clickable
+        const startButton = this.add.image(185, 450, 'startButton').setScale(.65).setInteractive({ cursor: 'pointer', pixelPerfect: true })
         
         // add hover effect
         startButton.on('pointerover', () => {
