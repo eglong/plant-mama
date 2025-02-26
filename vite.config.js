05c58ee906343bb996d82373236587c9a1334040
Emma Long <emmag2623@gmail.com>
2025-02-26 10:36:01 -0600

    setup gh-pages deploy with vite

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
+    base: "/plant-mama/", // Base URL for the project
     server: {
         open: true, // Auto-open browser on startup
     },
