97df4137bd1f1c6ec00f0bfd64c64128aeb845d8
Emma Long <emmag2623@gmail.com>
2025-02-27 22:00:46 -0600

    add scaling?

--- src/main.js
+++ src/main.js
@@ -6,12 +6,21 @@ import WaterScene from './scenes/WaterScene'
 
 const config = {
     type: Phaser.AUTO,
-    width: 622,
-    height: 648,
-    scene: [TitleScene, LevelScene, FertScene, WaterScene],
+    width: 600,
+    height: 600,
     scale: {
+        mode: Phaser.Scale.FIT,
         autoCenter: Phaser.Scale.CENTER_BOTH,
-    }
+        min: {
+            width: 300,
+            height: 300
+        },
+        max: {
+            width: 600,
+            height: 600
+        }
+    },
+    scene: [TitleScene, LevelScene, FertScene, WaterScene],
 };
 
 // Start the game with Phaser
